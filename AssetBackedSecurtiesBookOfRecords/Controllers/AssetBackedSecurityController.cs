e7bed5c9e1f0400e1cd242cc575fa502d3dc555d
San Kanao <sandeep.kanao@gmail.com>
2023-06-13 17:29:37 -0400

    Add files via upload

---
+++ AssetBackedSecurtiesBookOfRecords/Controllers/AssetBackedSecurityController.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using AssetBackedSecurity.Models;
+
+namespace AssetBackedSecurity.Controllers
+{
+    public class AssetBackedSecurityController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        // GET: ABS
+        [HttpGet]
+        public ActionResult Index(int i)
+        {
+           
+
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Case Officer")
+            {
+                // Case Officer can only view records
+                return View(GetAllABS());
+            }
+            else
+            {
+                // Manager and Administrator can add, update, and delete records
+                return View(GetAllABS());
+            }
+        }
+
+        // GET: ABS/Details/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Case Officer")
+            {
+                // Case Officer can only view records
+                AssetBackedSecurity.Models.AssetBackedSecurity abs = GetABSById(id);
+                if (abs == null)
+                {
+                    return NotFound();
+                }
+                return View(abs);
+            }
+            else
+            {
+                // Manager and Administrator can add, update, and delete records
+                AssetBackedSecurity.Models.AssetBackedSecurity abs = GetABSById(id);
+                if (abs == null)
+                {
+                    return NotFound();
+                }
+                return View(abs);
+            }
+        }
+
+        // GET: ABS/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Administrator" || role == "Manager")
+            {
+                return View();
+            }
+            else
+            {
+                // Case Officer is not allowed to create records
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: ABS/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AssetBackedSecurity.Models.AssetBackedSecurity abs)
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Administrator" || role == "Manager")
+            {
+                if (ModelState.IsValid)
+                {
+                    // Add the ABS record
+                    AddABS(abs);
+                    return RedirectToAction("Index");
+                }
+                return View(abs);
+            }
+            else
+            {
+                // Case Officer is not allowed to create records
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: ABS/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Administrator" || role == "Manager")
+            {
+                AssetBackedSecurity.Models.AssetBackedSecurity abs = GetABSById(id);
+                if (abs == null)
+                {
+                    return NotFound();
+                }
+                return View(abs);
+            }
+            else
+            {
+                // Case Officer is not allowed to edit records
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: ABS/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, AssetBackedSecurity.Models.AssetBackedSecurity abs)
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Administrator" || role == "Manager")
+            {
+                if (ModelState.IsValid)
+                {
+                    // Update the ABS record
+                    UpdateABS(id, abs);
+                    return RedirectToAction("Index");
+                }
+                return View(abs);
+            }
+            else
+            {
+                // Case Officer is not allowed to edit records
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: ABS/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Administrator")
+            {
+                AssetBackedSecurity.Models.AssetBackedSecurity abs = GetABSById(id);
+                if (abs == null)
+                {
+                    return NotFound();
+                }
+                return View(abs);
+            }
+            else
+            {
+                // Only Administrator can delete records
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: ABS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            // Check user role and perform appropriate actions
+            string role = GetUserRole(); // Get the role of the current user
+            if (role == "Administrator")
+            {
+                // Delete the ABS record
+                DeleteABS(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Only Administrator can delete records
+                return RedirectToAction("Index");
+            }
+        }
+
+        // Helper methods
+
+        // Get the role of the current user (Replace with actual implementation)
+        private string GetUserRole()
+        {
+            // Replace with actual implementation to get the role of the current user
+            return "Manager";
+        }
+
+        // Get all ABS records (Replace with actual implementation)
+        private List<AssetBackedSecurity.Models.AssetBackedSecurity> GetAllABS()
+        {
+            // Replace with actual implementation to get all ABS records
+            List<AssetBackedSecurity.Models.AssetBackedSecurity> absList = new List<AssetBackedSecurity.Models.AssetBackedSecurity>();
+            // Add code to populate the absList with data
+            // Ensure that the absList is not null before returning the view
+            //return View(absList);
+
+            //return new List<ABS>();
+            return absList;
+        }
+
+        // Get ABS record by ID (Replace with actual implementation)
+        private AssetBackedSecurity.Models.AssetBackedSecurity GetABSById(int id)
+        {
+            // Replace with actual implementation to get the ABS record by ID
+            return null;
+        }
+
+        // Add ABS record (Replace with actual implementation)
+        private void AddABS(AssetBackedSecurity.Models.AssetBackedSecurity abs)
+        {
+            // Replace with actual implementation to add the ABS record
+        }
+
+        // Update ABS record (Replace with actual implementation)
+        private void UpdateABS(int id, AssetBackedSecurity.Models.AssetBackedSecurity abs)
+        {
+            // Replace with actual implementation to update the ABS record
+        }
+
+        // Delete ABS record (Replace with actual implementation)
+        private void DeleteABS(int id)
+        {
+            // Replace with actual implementation to delete the ABS record
+        }
+    }
+}
---
+++ AssetBackedSecurtiesBookOfRecords/Models/AssetBackedSecurity.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssetBackedSecurity.Models
+{
+    // Asset-Backed Security (ABS) model
+    public class AssetBackedSecurity
+    {
+        // ABS properties
+        public int ABSId { get; set; } // Unique ID of the ABS
+        public string ABSName { get; set; } // Name of the ABS
+        public List<UnderlyingAsset> UnderlyingAssets { get; set; } // List of underlying assets
+        public List<CashFlow> CashFlows { get; set; } // List of cash flows
+        public List<PaymentSchedule> PaymentSchedules { get; set; } // List of payment schedules
+        public List<Tranche> Tranches { get; set; } // List of tranches
+        public IssuanceDetails IssuanceDetails { get; set; } // Issuance details
+        public ServicingDetails ServicingDetails { get; set; } // Servicing details
+        public PerformanceMetrics PerformanceMetrics { get; set; } // Performance metrics
+
+        // Other relevant properties
+
+        // Default constructor to initialize collections
+        public AssetBackedSecurity()
+        {
+            UnderlyingAssets = new List<UnderlyingAsset>();
+            CashFlows = new List<CashFlow>();
+            PaymentSchedules = new List<PaymentSchedule>();
+            Tranches = new List<Tranche>();
+            IssuanceDetails = new IssuanceDetails();
+            ServicingDetails = new ServicingDetails();
+            PerformanceMetrics = new PerformanceMetrics();
+        }
+    }
+
+    // Underlying Asset model
+    public class UnderlyingAsset
+    {
+        // Underlying asset properties
+        public int AssetId { get; set; } // Unique ID of the asset
+        public string AssetName { get; set; } // Name of the asset
+        public decimal AssetValue { get; set; } // Value of the asset
+        public DateTime PurchaseDate { get; set; } // Purchase date of the asset
+        public string PurchaseLocation { get; set; } // Purchase location of the asset
+
+        // Other properties related to the underlying asset
+    }
+
+    // Cash Flow model
+    public class CashFlow
+    {
+        // Cash flow properties
+        public int CashFlowId { get; set; } // Unique ID of the cash flow
+        public DateTime FlowDate { get; set; } // Date of the cash flow
+        public decimal FlowAmount { get; set; } // Amount of the cash flow
+        public string FlowType { get; set; } // Type of the cash flow
+
+        // Other properties related to the cash flow
+    }
+
+    // Payment Schedule model
+    public class PaymentSchedule
+    {
+        // Payment schedule properties
+        public int ScheduleId { get; set; } // Unique ID of the payment schedule
+        public DateTime PaymentDate { get; set; } // Date of the payment
+        public decimal PaymentAmount { get; set; } // Amount of the payment
+        public bool IsPaid { get; set; } // Flag indicating if the payment is paid
+
+        // Other properties related to the payment schedule
+    }
+
+    // Tranche model
+    public class Tranche
+    {
+        // Tranche properties
+        public int TrancheId { get; set; } // Unique ID of the tranche
+        public string TrancheName { get; set; } // Name of the tranche
+        public decimal PrincipalBalance { get; set; } // Principal balance of the tranche
+        public decimal InterestRate { get; set; } // Interest rate of the tranche
+        public DateTime MaturityDate { get; set; } // Maturity date of the tranche
+
+        // Other properties related to the tranche
+    }
+
+    // Issuance Details model
+    public class IssuanceDetails
+    {
+        // Issuance details properties
+        public string Issuer { get; set; } // Issuer of the ABS
+        public DateTime IssuanceDate { get; set; } // Date of issuance
+        public decimal TotalValue { get; set; } // Total value of the ABS
+        public string OfferingType { get; set; } // Type of offering
+
+        // Other properties related to the issuance details
+    }
+
+    // Servicing Details model
+    public class ServicingDetails
+    {
+        // Servicing details properties
+        public string Servicer { get; set; } // Servicer of the ABS
+        public DateTime StartDate { get; set; } // Start date of servicing
+        public DateTime EndDate { get; set; } // End date of servicing
+        public decimal FeePercentage { get; set; } // Fee percentage for servicing
+
+        // Other properties related to the servicing details
+    }
+
+    // Performance Metrics model
+    public class PerformanceMetrics
+    {
+        // Performance metrics properties
+        public decimal TotalPayments { get; set; } // Total payments received
+        public decimal PrincipalOutstanding { get; set; } // Principal outstanding
+        public decimal InterestReceived { get; set; } // Interest received
+
+        // Other properties related to the performance metrics
+    }
+}
